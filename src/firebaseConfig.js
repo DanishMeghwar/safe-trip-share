13647b7f1b6b498a8dae0f3737de5e4a7145f1ea
DanishMeghwar <danishkumarladhani@gmail.com>
2025-11-03 17:25:06 +0500

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDeErqkvvBfy_u13_a3CLeaMSFcEa9eAq8",
+  authDomain: "safe-trip-share.firebaseapp.com",
+  projectId: "safe-trip-share",
+  storageBucket: "safe-trip-share.firebasestorage.app",
+  messagingSenderId: "753357786741",
+  appId: "1:753357786741:web:11438217c9b75d22f1b23c",
+  measurementId: "G-NHN6K78HK8"
+}; 
