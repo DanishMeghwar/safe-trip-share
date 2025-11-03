3a0df9f9e8d125451182b16b5cb881b2348f59b6
DanishMeghwar <danishkumarladhani@gmail.com>
2025-11-03 16:57:13 +0500

    Create src/firebaseConfig.js

---
+++ src/src/firebaseConfig.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "safe-trip-share.firebaseapp.com",
+  projectId: "safe-trip-share",
+  storageBucket: "safe-trip-share.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const db = getFirestore(app);
+
+export { auth, googleProvider, db };
