0c1946bf88be4f159e9d03ac23da11f91011be78
DanishMeghwar <danishkumarladhani@gmail.com>
2025-11-07 23:03:57 +0500

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,4 +1,3 @@
-// src/firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -7,14 +6,13 @@ const firebaseConfig = {
   apiKey: "AIzaSyDeErqkvvBfy_u13_a3CLeaMSFcEa9eAq8",
   authDomain: "safe-trip-share.firebaseapp.com",
   projectId: "safe-trip-share",
-  storageBucket: "safe-trip-share.appspot.com",
+  storageBucket: "safe-trip-share.firebasestorage.app",
   messagingSenderId: "753357786741",
   appId: "1:753357786741:web:11438217c9b75d22f1b23c",
-  measurementId: "G-NHN6K78HK8"
+  measurementId: "G-NHN6K78HK8",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
+export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
